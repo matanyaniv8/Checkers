df64d9ff61d960e9e8456b8f4dd7e7fa447e1e1b
MatanYaniv <matanyaniv8@gmail.com>
2023-11-23 16:23:19 +0200

    issue with commit

---
+++ ChceckersLogicComponents/BoardCell.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections.Generic;
+
+namespace ChceckersLogicComponents
+{
+    public struct BoardCell
+    {
+        private const int k_DefualtAxisPoint = -1;
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public GameUtilities.ePlayerSign PlayerSign { get; set; }
+        public BoardCell DefualtCell
+        {
+            get
+            {
+                return new BoardCell(k_DefualtAxisPoint, k_DefualtAxisPoint);
+            }
+        }
+
+        public BoardCell(int i_XAxis=0, int i_YAxis=0, GameUtilities.ePlayerSign i_PlayerSign=GameUtilities.ePlayerSign.empty)
+        {
+            X = i_XAxis;
+            Y = i_YAxis;
+            PlayerSign = i_PlayerSign;
+        }
+
+        public BoardCell(List<int> i_Axes, GameUtilities.ePlayerSign i_PlayerSign = GameUtilities.ePlayerSign.empty)
+        {
+            if( i_Axes.Count >= 2)
+            {
+                X = i_Axes[0];
+                Y = i_Axes[1];
+                PlayerSign = i_PlayerSign;
+            }
+            else
+            {
+                throw new System.Exception("Not enough parameters");
+            }
+        }
+
+        public static bool operator ==(BoardCell lhs, BoardCell rhs)
+        {
+            return (lhs.X == rhs.X && lhs.Y == rhs.Y && lhs.PlayerSign == rhs.PlayerSign);
+        }
+        public static bool operator !=(BoardCell lhs, BoardCell rhs)
+        {
+            return !(lhs.X == rhs.X && lhs.Y == rhs.Y && lhs.PlayerSign == rhs.PlayerSign);
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool areEquals = false;
+            BoardCell otherCell = new BoardCell().DefualtCell;
+
+            if (obj != null || GetType() == obj.GetType())
+            {
+                areEquals = ((BoardCell)(obj) == this);
+            }
+
+            return areEquals;
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode() ^ PlayerSign.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return ($"X: {this.X}, Y: {this.Y}, Sign: {this.PlayerSign}");
+        }
+    }
+}
---
+++ ChceckersLogicComponents/GameUtilities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChceckersLogicComponents
+{
+    public static class GameUtilities
+    {
+        private const string k_NotValidEnumType = "Not Acceptable Value Inserted!";
+        public enum ePlayersType
+        {
+            Human = 0,
+            Computer = 1
+        }
+
+        public enum ePlayerSign
+        {
+            empty = 0,
+            first= 1,
+            second = 2
+        }
+
+        public static ePlayerSign GetPlayerSign(int i_PotentialPlayerSign)
+        {
+            ePlayerSign playerSignConverted;
+            bool isPlayerSignConverted = Enum.TryParse(i_PotentialPlayerSign.ToString(), out playerSignConverted);
+
+            if (!isPlayerSignConverted)
+            {
+                throw new Exception(k_NotValidEnumType);
+            }
+
+            return playerSignConverted;
+        }
+
+        public static ePlayersType GetPlayersType(bool i_PlayerType)
+        {
+            ePlayersType playerTypeConverted = ePlayersType.Human;
+
+            if(i_PlayerType == false)
+            {
+                playerTypeConverted = ePlayersType.Computer;
+            }
+
+            return playerTypeConverted;
+        }
+    }
+}
