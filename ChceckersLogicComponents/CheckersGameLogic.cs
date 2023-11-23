6f600ea3402060449def94de04ccc24500c81925
MatanYaniv <matanyaniv8@gmail.com>
2023-11-23 16:26:52 +0200

    resolved an issue with too much access to Board class from the Logic Class.

---
+++ ChceckersLogicComponents/CheckersGameLogic.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChceckersLogicComponents
+{
+    public class CheckersGameLogic
+    {
+        private RandomMoveGenerator r_RandomMoveGenerator = null;
+        private const int k_NumberOfTroops = 12;
+        private const string k_NotCorrectPlayerHasChosenMessage = "please choose a soldier from your troops";
+        public Player FirstPlayer { get; private set; }
+        public Player SecondPlayer { get; private set;}
+        internal Board CheckersBoard { get; private set; }
+        public Player CurrentPlayerTurn { get; private set; }
+        public Player OpponentPlayer { get; private set; }
+        public int BoardSize { get; private set; }
+
+        public CheckersGameLogic(string i_FirstPlayerName, string i_SecondPlayerName="Computer", bool i_IsSecondPlayerHuman=false)
+        {
+            CheckersBoard = new Board(i_IsSecondPlayerHuman);
+            BoardSize = CheckersBoard.BoardSize;
+            FirstPlayer = new Player(i_FirstPlayerName,1 ,true, k_NumberOfTroops);
+            FirstPlayer.PlayerSign = GameUtilities.ePlayerSign.first;
+            SecondPlayer = new Player(i_SecondPlayerName, 2, i_IsSecondPlayerHuman, k_NumberOfTroops);
+            SecondPlayer.PlayerSign = GameUtilities.ePlayerSign.second;
+            CurrentPlayerTurn = FirstPlayer;
+            OpponentPlayer = SecondPlayer;
+            CheckersBoard.InitializeBoard(FirstPlayer.PlayerSign, SecondPlayer.PlayerSign) ;
+
+            if(!i_IsSecondPlayerHuman)
+            {
+                r_RandomMoveGenerator = new RandomMoveGenerator(SecondPlayer, CheckersBoard);
+            }
+        }
+
+        public Player MakeAMove(BoardCell i_CurrentPlayerTroopLocation, BoardCell i_SelectedLocationOnBoard )
+        {
+            Player lastPlayedPlayer = CurrentPlayerTurn;
+
+            if (!IsThereAWin())
+            {
+                if (CurrentPlayerTurn.PlayerSign == i_CurrentPlayerTroopLocation.PlayerSign)
+                {
+                    CheckersBoard.MakeAMove(CurrentPlayerTurn, OpponentPlayer, i_CurrentPlayerTroopLocation, i_SelectedLocationOnBoard);
+                }
+                else
+                {
+                    throw new Exception(k_NotCorrectPlayerHasChosenMessage + $" - {CurrentPlayerTurn.Name}");
+                }
+            }
+
+            CurrentPlayerTurn = OpponentPlayer;
+            OpponentPlayer = lastPlayedPlayer;
+
+            return lastPlayedPlayer;
+        }
+
+        public void MakeRandomMove()
+        {
+            KeyValuePair<BoardCell, BoardCell> currentAndTargetCells = r_RandomMoveGenerator.GenerateRandomMove();
+
+            if(SecondPlayer.PlayerType == GameUtilities.ePlayersType.Computer 
+                && currentAndTargetCells.Key != currentAndTargetCells.Value)
+            {
+                MakeAMove(currentAndTargetCells.Key, currentAndTargetCells.Value);
+            }
+        }
+
+        public GameUtilities.ePlayerSign GetCellValue(int i_RowIndex, int i_ColIndex)
+        {
+           return CheckersBoard.GetPlayerSignFromBoardCell(new BoardCell(i_RowIndex, i_ColIndex));
+        }
+
+        public bool IsThereAWin()
+        {
+            return isFirstPlayerWinning() || isSecondPlayerWinning();
+        }
+
+        private bool isFirstPlayerWinning()
+        {
+            return SecondPlayer.NumberOfTroopsRemaining == 0;
+        }
+        private bool isSecondPlayerWinning()
+        {
+            return FirstPlayer.NumberOfTroopsRemaining == 0;
+        }
+    }
+}
---
+++ CheckersWinform/Program.cs
@@ -0,0 +1,149 @@
+﻿using ChceckersLogicComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckersWinform
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckersGameLogic board = new CheckersGameLogic("Matan","Computer" ,false);
+            run(board);
+        }
+
+        private static void run(CheckersGameLogic i_GameBoard)
+        {
+            BoardCell[] indices = null;
+            Player lastPlayer = i_GameBoard.CurrentPlayerTurn;
+            bool isThereAWin = false;
+            printBoard(i_GameBoard);
+
+            while (!isThereAWin)
+            {
+                isThereAWin = i_GameBoard.IsThereAWin();
+                lastPlayer = i_GameBoard.CurrentPlayerTurn;
+
+                if (lastPlayer.PlayerType == GameUtilities.ePlayersType.Computer)
+                {
+                    try
+                    {
+                        i_GameBoard.MakeRandomMove();
+                        Console.WriteLine("Computer Move:");
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+
+                else
+                {
+                    try
+                    {
+                        indices = askUserForAMove(i_GameBoard);
+                        lastPlayer = i_GameBoard.MakeAMove(indices[0], indices[1]);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                printBoard(i_GameBoard);
+            }
+
+            Console.WriteLine($"The Winner is {lastPlayer.Name}");
+        }
+
+        private static void printBoard(CheckersGameLogic i_GameBoardToPrint)
+        {
+            Console.WriteLine(columnsNumbers(i_GameBoardToPrint.BoardSize));
+
+            if(i_GameBoardToPrint != null)
+            {
+                for(int i = 0; i < i_GameBoardToPrint.BoardSize; i++)
+                {
+                    Console.WriteLine(getLineFromBoard(i_GameBoardToPrint, i));
+                }
+            }
+        }
+        private static string columnsNumbers(int i_BoardSize)
+        {
+            StringBuilder columnsNumbers = new StringBuilder();
+
+            for (int i = 1; i <= i_BoardSize; i++)
+            {
+                if (i == 1)
+                {
+                    columnsNumbers.Append("  " + i.ToString() + "   ");
+                }
+                else
+                {
+                    columnsNumbers.Append(i.ToString() + "   ");
+                }
+            }
+
+            return columnsNumbers.ToString();
+        }
+
+        private static string getLineFromBoard(CheckersGameLogic i_GameBoardToPrint, int i_LineNumber) 
+        {
+            StringBuilder boardLine = new StringBuilder();
+            GameUtilities.ePlayerSign currentSlotValue = GameUtilities.ePlayerSign.empty;
+            string defualtSlotSign = "   |";
+            string slotSignToPrint = defualtSlotSign;
+
+            boardLine.Append((i_LineNumber + 1) + "|");
+
+            for (int i = 0; i< i_GameBoardToPrint.BoardSize; i++)
+            {
+                slotSignToPrint = defualtSlotSign;
+                currentSlotValue = i_GameBoardToPrint.GetCellValue(i_LineNumber, i);
+
+                if(currentSlotValue != GameUtilities.ePlayerSign.empty)
+                {
+                    slotSignToPrint = (currentSlotValue == GameUtilities.ePlayerSign.first) ? "X  |" : "O  |";
+                }
+
+                boardLine.Append(slotSignToPrint);
+            }
+
+            return boardLine.ToString();
+        }
+
+        private static BoardCell[] askUserForAMove(CheckersGameLogic i_CheckersGame)
+        {
+            string userTroopAnswer = null;
+            string userMoveLocation = null;
+
+            Console.WriteLine("Pls choose a troop that will make the move");
+            userTroopAnswer = Console.ReadLine();
+            Console.WriteLine("Please choose a legit location");
+            userMoveLocation =  Console.ReadLine();
+            
+            return new BoardCell[] {getUserLocation(i_CheckersGame, userTroopAnswer), getUserLocation(i_CheckersGame, userMoveLocation)};
+        }
+
+
+        private static BoardCell getUserLocation(CheckersGameLogic i_CheckersGame,string i_UserChoiseAsString, char i_AnswerDividerSign = ',')
+        {
+            List<int> userChoiceAsInt = new List<int>();
+
+            foreach (string index in i_UserChoiseAsString.Split(i_AnswerDividerSign))
+            {
+                try
+                {
+                    userChoiceAsInt.Add(int.Parse(index) -1);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return new BoardCell(userChoiceAsInt, i_CheckersGame.GetCellValue(userChoiceAsInt[0], userChoiceAsInt[1]));
+        }
+    }
+}
